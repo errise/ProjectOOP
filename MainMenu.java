b0652365e781d10118876bdc6299d7b61e08603b
Scross000 <k.hartarto9@gmail.com>
2016-10-18 20:26:14 +0700

    Update MainMenu.java

--- MainMenu.java
+++ MainMenu.java
@@ -14,7 +14,7 @@ public class MainMenu {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner s = new Scanner(System.in);
     }
     
 }
