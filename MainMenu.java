5bbd4f9dbf5ac63e2d6e06cb3ffcaa3b144c767b
flying stone <k.hartarto9@gmail.com>
2016-10-19 11:53:21 +0700

    penambahan menu
    tambahan menu

--- MainMenu.java
+++ MainMenu.java
@@ -1,3 +1,7 @@
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -19,6 +23,13 @@ public class MainMenu {
         LinkedList<TokoBuku> daftarTokoBuku = new LinkedList();
         String menu = " ";
         
+        do {
+            System.out.println("E-Bookstore");
+            System.out.println("[1] User");
+            System.out.println("[2] BookStore Admin");
+            System.out.println("[3] Admin");
+            System.out.println("[4] Exit");
+        } while (!("4").equals(menu));
         
     }
     
