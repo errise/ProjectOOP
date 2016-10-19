14c9b50f40a5269d3a23a88ef6742691960e906e
flying stone <k.hartarto9@gmail.com>
2016-10-19 18:09:52 +0700

    nambah property tokoBuku

--- PROJECT_OOP_E-Bookstore/src/TokoBuku.java
+++ PROJECT_OOP_E-Bookstore/src/TokoBuku.java
@@ -1,3 +1,6 @@
+
+import java.util.LinkedList;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +12,10 @@
  * @author Kevin
  */
 public class TokoBuku {
-    
+    private String kota;
+    private String idTokoBuku;
+    private String alamat;
+    private String noTelepon;
+    private LinkedList<Buku> daftarBuku;
+    private LinkedList<Pesanan> daftarPesanan;
 }
