e1fb2d915ea3145a22bb560ae47009924383131c
flying stone <k.hartarto9@gmail.com>
2016-10-20 13:21:01 +0700

    penamabahan class
    menambahkan method ke clas toko buku dan membuat class pesanan

--- PROJECT_OOP_E-Bookstore/src/Pesanan.java
+++ PROJECT_OOP_E-Bookstore/src/Pesanan.java
@@ -1,3 +1,6 @@
+
+import java.util.LinkedList;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +12,120 @@
  * @author Kevin
  */
 public class Pesanan {
+    private String idPesanan;
+    private LinkedList<Buku> daftarBuku;
+    private int[] daftarJumlahBukuYangDipesan;
+    private String idUser;
+    private String idTokoBuku;
+    private String namaTokoBuku;
+    private String namaUser;
+    private boolean status;
+    private double totalHarga;
+
+    public Pesanan() {
+        this.idPesanan = "Tidak ada";
+        this.daftarBuku = new LinkedList();
+        this.daftarJumlahBukuYangDipesan = new int[10];
+        this.idUser = "Tidak ada";
+        this.idTokoBuku = "Tidak ada";
+        this.namaTokoBuku = "Tidak ada";
+        this.namaUser = "Tidak ada";
+        this.status = false;
+        this.totalHarga = 0;
+    }
+
+    public String getIdPesanan() {
+        return idPesanan;
+    }
+
+    public void setIdPesanan(String idPesanan) {
+        this.idPesanan = idPesanan;
+    }
+
+    public LinkedList<Buku> getDaftarBuku() {
+        return daftarBuku;
+    }
+
+    public void setDaftarBuku(LinkedList<Buku> daftarBuku) {
+        this.daftarBuku = daftarBuku;
+    }
+
+    public int[] getDaftarJumlahBukuYangDipesan() {
+        return daftarJumlahBukuYangDipesan;
+    }
+
+    public void setDaftarJumlahBukuYangDipesan(int[] daftarJumlahBukuYangDipesan) {
+        this.daftarJumlahBukuYangDipesan = daftarJumlahBukuYangDipesan;
+    }
+
+    public String getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(String idUser) {
+        this.idUser = idUser;
+    }
+
+    public String getIdTokoBuku() {
+        return idTokoBuku;
+    }
+
+    public void setIdTokoBuku(String idTokoBuku) {
+        this.idTokoBuku = idTokoBuku;
+    }
+
+    public String getNamaTokoBuku() {
+        return namaTokoBuku;
+    }
+
+    public void setNamaTokoBuku(String namaTokoBuku) {
+        this.namaTokoBuku = namaTokoBuku;
+    }
+
+    public String getNamaUser() {
+        return namaUser;
+    }
+
+    public void setNamaUser(String namaUser) {
+        this.namaUser = namaUser;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public double getTotalHarga() {
+        return totalHarga;
+    }
+
+    public void setTotalHarga(double totalHarga) {
+        this.totalHarga = totalHarga;
+    }
     
+    public double hitungTotalHarga(){
+        double total = 0;
+        for (int i = 0; i < daftarBuku.size(); i++) {
+            total = total +(daftarBuku.get(i).getHarga()*daftarJumlahBukuYangDipesan[i]);
+        }
+        return total;
+    }
+        
+    public void show(){
+        System.out.println("Id Pesanan      : "+this.idPesanan);
+        System.out.println("Pemesan         : "+this.namaUser);
+        System.out.println("Dipesan dari    : "+this.namaTokoBuku);
+        for (int i = 0; i < daftarBuku.size(); i++) {
+            System.out.println((i+1)+". "+daftarBuku.get(i).getJudulBuku()+" x"+daftarJumlahBukuYangDipesan[i]);
+        }
+        System.out.println("Total harga     : Rp "+hitungTotalHarga());
+        if (status == false) {
+            System.out.println("Status lunas    : Belum lunas");
+        }else{
+            System.out.println("Status lunas    : Lunas");
+        }
+    }
 }
--- PROJECT_OOP_E-Bookstore/src/TokoBuku.java
+++ PROJECT_OOP_E-Bookstore/src/TokoBuku.java
@@ -6,12 +6,12 @@ import java.util.LinkedList;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Kevin
  */
 public class TokoBuku {
+
     private String idTokoBuku;
     private String kota;
     private String password;
@@ -31,7 +31,7 @@ public class TokoBuku {
     }
 
     public TokoBuku() {
-        this.kota ="Tidak ada";
+        this.kota = "Tidak ada";
         this.idTokoBuku = "Tidak ada";
         this.password = "Tidak ada";
         this.alamat = "Tidak ada";
@@ -87,36 +87,86 @@ public class TokoBuku {
     public void setDaftarPesanan(LinkedList<Pesanan> daftarPesanan) {
         this.daftarPesanan = daftarPesanan;
     }
-    
-    public void addBuku(Buku bukuBaru){
+
+    public void addBuku(Buku bukuBaru) {
         if (daftarBuku.isEmpty()) {//daftar buku masih kosong
             daftarBuku.addFirst(bukuBaru);//Memasukkan buku baru ke dalam daftar buku dari depan
-        }else{//daftar buku tidak kosong
+        } else {//daftar buku tidak kosong
             daftarBuku.add(bukuBaru);//Memasukkan buku baru ke dalam daftar buku
         }
     }
-    
-    public void editBuku(Buku bukuEdit){
+
+    public void editBuku(Buku bukuEdit) {
         if (daftarBuku.contains(bukuEdit)) {//daftar buku memiliki buku yang ingin diedit
             daftarBuku.set(daftarBuku.indexOf(bukuEdit), bukuEdit);//Mengganti buku yang ada di daftar buku dengan buku yang baru
         }
     }
-    
-    public void hapusBuku(Buku bukuHapus){
+
+    public void hapusBuku(Buku bukuHapus) {
         if (daftarBuku.contains(bukuHapus)) {//daftar buku memiliki buku yang ingin dihapus
             daftarBuku.remove(daftarBuku.indexOf(bukuHapus));//Menghapus buku yang ada di daftar buku
         }
     }
-    
-    public void tambahStok(Buku tambahBuku, int tambahStok){
+
+    public void tambahStok(Buku tambahBuku, int tambahStok) {
         if (daftarBuku.contains(tambahBuku)) {//daftar buku memiliki buku yang ingin ditambah stoknya
-            daftarBuku.get(daftarBuku.indexOf(tambahBuku)).setStok(daftarBuku.get(daftarBuku.indexOf(tambahBuku)).getStok()+tambahStok);//menambahkan stok buku dengan jumlah yang diinputkan
+            daftarBuku.get(daftarBuku.indexOf(tambahBuku)).setStok(daftarBuku.get(daftarBuku.indexOf(tambahBuku)).getStok() + tambahStok);//menambahkan stok buku dengan jumlah yang diinputkan
+        }
+    }
+
+    public void kurangStok(Buku kurangBuku, int kurangStok) {
+        if (daftarBuku.contains(kurangBuku)) {//daftar buku memiliki buku yang ingin dikurangi stoknya
+            daftarBuku.get(daftarBuku.indexOf(kurangBuku)).setStok(daftarBuku.get(daftarBuku.indexOf(kurangBuku)).getStok() - kurangStok);//mengurangkan stok buku dengan jumlah yang diinputkan
+        }
+    }
+
+    public void editTokoBuku(String kota, String alamat, String noTelepon) {
+        this.kota = kota;
+        this.alamat = alamat;
+        this.noTelepon = noTelepon;
+    }
+
+    public void batalPesanan(Pesanan pesananBatal) {
+        if (daftarPesanan.contains(pesananBatal)) {//daftar pesanan memiliki pesanan yang ingin dibatalkan
+            daftarPesanan.remove(pesananBatal);//membatalkan pesanan dan menghapusnya
+        }
+    }
+
+    public void pesananLunas(Pesanan pesananLunas) {
+        if (daftarPesanan.contains(pesananLunas)) {//daftar pesanan memiliki pesanan yang mau diubah status 
+            if (daftarPesanan.get(daftarPesanan.indexOf(pesananLunas)).isStatus() == false) {//pesanan belum lunas
+                daftarPesanan.get(daftarPesanan.indexOf(pesananLunas)).setStatus(true);//mengubah status pesanan menjadi lunas
+            }
+        }
+    }
+
+    public Buku cariBuku(String judulTokoBuku) {
+        Buku bukuDicari = null;
+        for (int i = 0; i < daftarBuku.size(); i++) {
+            if (daftarBuku.get(i).getJudulBuku().equals(judulTokoBuku)) {
+                bukuDicari = daftarBuku.get(i);
+            }
+        }
+        return bukuDicari;
+    }
+
+    public void lihatDaftarBuku() {
+        for (int i = 0; i < daftarBuku.size(); i++) {
+            if (daftarBuku.get(i).isStatus()) {
+                System.out.println((i + 1) + ". " + daftarBuku.get(i).getJudulBuku() + " - " + daftarBuku.get(i).getPenulis() + ", Tersedia");
+            } else {
+                System.out.println((i + 1) + ". " + daftarBuku.get(i).getJudulBuku() + " - " + daftarBuku.get(i).getPenulis() + ", Tidak tersedia");
+            }
         }
     }
     
-    public void kurangStok(Buku kurangBuku, int kurangStok){
-        if (daftarBuku.contains(kurangBuku)) {//daftar buku memiliki buku yang ingin dikurangi stoknya
-            daftarBuku.get(daftarBuku.indexOf(kurangBuku)).setStok(daftarBuku.get(daftarBuku.indexOf(kurangBuku)).getStok()-kurangStok);//mengurangkan stok buku dengan jumlah yang diinputkan
+    public void lihatDaftarPesanan(){
+        for (int i = 0; i < daftarPesanan.size(); i++) {
+            if (daftarPesanan.get(i).isStatus()) {
+                System.out.println((0+1)+". ["+daftarPesanan.get(i).getIdPesanan()+"] "+daftarPesanan.get(i).getNamaUser()+" - Rp"+daftarPesanan.get(i).getTotalHarga()+" - Lunas");
+            }else{
+                System.out.println((0+1)+". ["+daftarPesanan.get(i).getIdPesanan()+"] "+daftarPesanan.get(i).getNamaUser()+" - Rp"+daftarPesanan.get(i).getTotalHarga()+" - Belum lunas");
+            }
         }
     }
 }
